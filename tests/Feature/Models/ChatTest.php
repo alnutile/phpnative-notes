3aa3147683828d5db4c40ce600ded67679b0f3c6
alnutile <alfrednutile@gmail.com>
2023-12-24 20:45:09 -0500

    just see how this code works from this other app

---
+++ tests/Feature/Models/ChatTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Chat;
+use App\Models\Message;
+use AssertionError;
+use http\Client;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use OpenAI\Laravel\Facades\OpenAI;
+use OpenAI\Responses\Chat\CreateResponse;
+use OpenAI\Responses\Chat\CreateStreamedResponse;
+use OpenAI\Responses\Completions\CreateResponse as CompletionResponse;
+use OpenAI\Testing\ClientFake;
+use Tests\TestCase;
+
+class ChatTest extends TestCase
+{
+
+    public function test_start_chat() {
+
+        OpenAI::fake(
+            [
+                CompletionResponse::fake([
+                    'choices' => [
+                        [
+                            'text' => 'awesome!',
+                        ],
+                    ],
+                ]),
+            ]
+        );
+
+
+        $chat = Chat::factory()->create();
+
+        $message = $chat->addInput(
+            'testing'
+        );
+
+        $this->assertInstanceOf(Message::class, $message);
+
+        $this->assertEquals('awesome!', $chat->refresh()->title);
+    }
+
+    public function test_output() {
+
+        OpenAI::fake(
+            [
+                CreateResponse::fake([
+                    'choices' => [
+                        [
+                            'text' => 'awesome!',
+                        ],
+                    ],
+                ])
+            ]
+        );
+
+
+        $chat = Chat::factory()->create();
+
+        /**
+         * Create the starting message
+         */
+        Message::factory()->create([
+            'chat_id' => $chat->id
+        ]);
+
+        $this->assertDatabaseCount('messages', 1);
+
+        $message = $chat->addOutput(
+            'testing'
+        );
+
+        $this->assertInstanceOf(Message::class, $message);
+
+        $this->assertDatabaseCount('messages', 2);
+    }
+}
